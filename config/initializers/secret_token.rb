39424195bb9b4c586592362726ee05622cf4c228
Zach Serafini <zserafini@covermymeds.com>
2013-11-16 10:40:19 -0500

    Initial setup of rails framework

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mule::Application.config.secret_token = 'a56ec5ab1ac91705fdc9cea769543557656bbf6ea9ba86041f30d8255d7a3ba8ff154d8e9622386c06bdbcea0661e2dc37b7d04d2f60314f969c9ac3ce5899b4'
